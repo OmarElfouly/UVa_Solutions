26419ede2e413b2011429cd059b661b8fdb12e25
omarelfouly <omarelfouly@aucegypt.edu>
2023-10-26 14:40:28 +0300

    Fixed 11614

--- 11614.cpp
+++ 11614.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 
 int main(){
-    int c,n;
+    int c;
+    long long n;
     scanf("%d", &c);
     while(c--){
-        scanf("%d", &n);
-        printf("%d\n", int (floor(-0.5 + sqrt(0.25+2*n))));
+        scanf("%lld", &n);
+        printf("%lld\n", (long long) (floor(-0.5 + sqrt(0.25+2*n))));
     }
+    return 0;
 }
