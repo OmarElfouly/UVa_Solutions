8d7d71d5557e0cf6c8b18296c4cde92d961957dc
omarelfouly <omarelfouly@aucegypt.edu>
2023-10-30 19:24:02 +0200

    Completed 11172

---
+++ 11172.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    scanf("%d",&t);
+    int a,b;
+    while(t--){
+        scanf("%d %d", &a,&b);
+        if(a<b) cout << "<\n";
+        else if(a>b) cout <<">\n";
+        else cout << "=\n";
+    }
+    return 0;
+}
