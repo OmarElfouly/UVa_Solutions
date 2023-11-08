1bae5e374c5673d0cde2f6f87c0f41de5fb1d059
omarelfouly <omarelfouly@aucegypt.edu>
2023-11-08 10:35:45 +0200

    Adds second solution to 11332

--- 11332.cpp
+++ 11332.cpp
@@ -1,19 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int digitSum(int n){
+    if(n<10) return n;
+    int sum = 0;
+    while(n){
+        sum += n%10;
+        n /= 10;
+    }
+    return digitSum(sum);
+}
+
 int main(){
     int n;
     cin >> n;
     while(n){
-        while(n>9){
-        int sum = 0;
-        while(n){
-            sum += n%10;
-            n /= 10;
-        }
-        n = sum;
-        }
-        cout << n << "\n";
+        // while(n>9){
+        // int sum = 0;
+        // while(n){
+        //     sum += n%10;
+        //     n /= 10;
+        // }
+        // n = sum;
+        // }
+        // cout << n << "\n";
+        cout << digitSum(n) << "\n";
         cin >> n;
     }
     
